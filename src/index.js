8cd916b4261508cd0f14c748f83e820f08586ef2
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-17 15:44:07 -0700

    Add Provider wrapper and import store.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provier} from 'react-redux';
 
 import './reset.css';
 import './index.css';
+import {createStore} from './index';
 
 import Game from './components/game';
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+    <Provider store={createStore}>
+      <Game />
+    </Provider>,
+      document.getElementById('root')
 );
